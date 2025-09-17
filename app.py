7a7ac4cc9614fbddc54c2a91846a4f2ece0215c0
jcmartin-gh <119459184+jcmartin-gh@users.noreply.github.com>
2025-09-17 21:25:53 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ import pandas as pd
 
 # ==== Google Drive (Service Account) ====
 from typing import List, Dict, Optional
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
-from googleapiclient.http import MediaIoBaseDownload
+#from google.oauth2 import service_account
+#from googleapiclient.discovery import build
+#from googleapiclient.http import MediaIoBaseDownload
 
 # ==== Whisper ====
 from faster_whisper import WhisperModel
